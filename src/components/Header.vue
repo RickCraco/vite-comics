c359a39851b27ff32a82910cd7027890590c7e4e
RickCraco <riccardocracolici43@gmail.com>
2023-11-09 15:16:29 +0100

    aggiunto template base + array di stringhe per il menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,22 @@
 <template>
-    
+    <header>
+        <div class="container">
+            <img src="../../public/img/dc-logo.png" alt="logo">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </header>
 </template>
 
 <script lang="ts">
     export default {
-        
+        name : 'Header',
+        data(){
+            return {
+                menu : ['CHARACTERS','COMICS','MOVIES', 'TV','GAMES','COLLECTIBLES', 'VIDEO','FANS','NEWS', 'SHOP']
+            }
+        }
     }
 </script>
 
