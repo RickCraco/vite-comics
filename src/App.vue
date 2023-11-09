ae7b4483367d2eaf1e453e865b7c9f1282b65187
RickCraco <riccardocracolici43@gmail.com>
2023-11-09 16:48:40 +0100

    aggiunto componente main

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <Header/>
+  <Main/>
 </template>
 
 <script>
   import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
+
   export default {
     name : 'App',
     components: {
-      Header
+      Header,
+      Main
     }
   }
 </script>
