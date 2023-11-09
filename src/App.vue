74763d6956e57dfe266658d728db5405677381f8
RickCraco <riccardocracolici43@gmail.com>
2023-11-09 17:38:33 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Header/>
   <Main/>
+  <Footer/>
 </template>
 
 <script>
   import Header from './components/Header.vue';
   import Main from './components/Main.vue';
+  import Footer from './components/Footer.vue';
 
   export default {
     name : 'App',
     components: {
       Header,
-      Main
+      Main,
+      Footer
     }
   }
 </script>
